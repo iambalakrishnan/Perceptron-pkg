df982c63657be3ebc10e590803a7c85896996d87
iambalakrishnan <imbkrishnaa@gmail.com>
2022-11-30 15:25:01 +0530

    update to 0.0.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ USER_NAME = "iambalakrishnan"
 
 setuptools.setup(
     name=f"{PACKAGE_NAME}",
-    version="0.0.3",
+    version="0.0.4",
     author=USER_NAME,
     author_email="imbkrishnaa@gmail.com",
     description="A small package for perceptron",
